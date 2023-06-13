c7d2cfb744c676f072d1c72ae495b0e2e4a89c90
Nate Pedersen <nate@fullfili.com>
2023-06-13 10:26:15 -0600

    Login Page Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Fullfili</h1>
-    </div>
+    <Authenticator>
+      <div className="App">
+        <h1>Welcome to Fullfili</h1>
+      </div>
+    </Authenticator>
   );
 }
 
